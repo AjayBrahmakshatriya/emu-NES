c6d94972de635978d1da87a8189fbcb07b71017e
Ajay Brahmakshatriya <a.ajay9470@gmail.com>
2017-10-02 13:59:06 +0530

    Moved SDL event handling to separate thread to avoid not responding error

--- include/display.h
+++ include/display.h
@@ -10,6 +10,7 @@
 typedef struct {
 	SDL_Window *window;
 	SDL_Surface *surface;
+	SDL_Thread *event_thread;
 }NES_DISPLAY;
 
 
--- src/display.c
+++ src/display.c
@@ -1,10 +1,22 @@
 #include "display.h"
 
+static int event_thread(void *param) {
+	NES_DISPLAY *nes_display = (NES_DISPLAY*) param;
+	SDL_Event events;
+	while(SDL_WaitEvent(&events)) {
+        	if(events.type == SDL_QUIT)
+            		exit(0);
+    	}
+	return 0;	
+}
+
 
 NES_DISPLAY* create_nes_display(void) {
 	NES_DISPLAY *nes_display = malloc(sizeof(*nes_display));
 	nes_display->window = SDL_CreateWindow("emu-NES", 100, 100, DISPLAY_WIDTH, DISPLAY_HEIGHT, SDL_WINDOW_SHOWN);
 	nes_display->surface = SDL_GetWindowSurface(nes_display->window);
+	nes_display->event_thread = SDL_CreateThread(event_thread, "EventThread", (void *)nes_display);
+
 	return nes_display;
 }
 
@@ -29,10 +41,5 @@ int update_display(NES_DISPLAY *nes_display, unsigned char *output_buffer) {
                 0);
 	SDL_BlitSurface(image, NULL, nes_display->surface, NULL);
 	SDL_UpdateWindowSurface(nes_display->window);
-	SDL_Event events;
-	while(SDL_PollEvent(&events)) {
-        	if(events.type == SDL_QUIT)
-            		exit(0);
-    	}
 	return 0;
 }
